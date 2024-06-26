00d6059d1fd06d4b47d8ff1934b59175153b52d3
Gourav Sharma <gourav.sharma.fiem.cse19@teamfuture.in>
2024-06-26 22:29:13 +0530

    1.01v Home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/layout/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
+import {SkillsComponent} from './components/skills/skills.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,HomeComponent],
+  imports: [RouterOutlet,HeaderComponent,HomeComponent,SkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
