cf4b5ac6b4e67f264d77cc21171b8eaefdabcf63
WHITE DEVIL <gourav.sharma.fiem.cse19@teamfuture.in>
2024-06-30 22:33:27 +0530

    bug fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from './shared/layout/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
 import {SkillsComponent} from './components/skills/skills.component';
 import { ExperienceComponent } from './components/experience/experience.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,HomeComponent,SkillsComponent,ExperienceComponent],
+  imports: [RouterOutlet,HeaderComponent,HomeComponent,SkillsComponent,ExperienceComponent,ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/shared/layout/header/header.component.ts
+++ src/app/shared/layout/header/header.component.ts
@@ -10,20 +10,28 @@ import { ThemeChangeComponent } from '../../../components/theme-change/theme-cha
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-  public sidenav:boolean = false;
+ public sidenav: boolean = false;
   public Menu: IMenu[] = [
-    { id: 1, name: "Home", url: '' },
-    { id: 2, name: "Project", url: 'project' },
-    { id: 3, name: "Skills", url: '#skills' },
-    { id: 4, name: "Expricence", url: '#expricence' },
-    { id: 5, name: "contact", url: '#contact' }
+    { id: 1, name: 'Home', url: 'home' },
+    { id: 2, name: 'Skills', url: 'skills' },
+    { id: 3, name: 'Project', url: 'project' },
+    { id: 4, name: 'Experience', url: 'experience' },
+    { id: 5, name: 'Contact', url: 'contact' }
   ];
+
   toggleMenu() {
-      this.sidenav = !this.sidenav;
+    this.sidenav = !this.sidenav;
+  }
+
+  scrollToSection(sectionId: string) {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
     }
   }
+}
 interface IMenu{
   id: number,
   name: string,
-  url: string|null,
+  url: string,
 }
